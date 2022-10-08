85c34e042df5c976f3dc1cc7ce1f8596557b1efe
unknown <muhammedslah098765@gmail.com>
2022-10-08 22:26:41 +0300

    adding userList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import AddUser from "./Components/Users/AddUser";
+import UserList from "./Components/Users/UserList";
 
 const App = () => {
+
+  
+
   return (
       <div className="App">
-        
+        <AddUser />
+        <UserList  /> 
       </div>
   );
 };
